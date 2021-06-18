5220f28729ef8d737e3d5013243c9768fd1aab59
FilipTarajko <filiptarajko@gmail.com>
2021-06-18 10:27:18 +0200

    prepare for heroku deplyoment

--- app.js
+++ app.js
@@ -10,7 +10,8 @@ var helmet = require('helmet');
 
 var app = express();
 var mongoose = require('mongoose');
-var mongoDB = 'mongodb+srv://store123:store321@cluster0.ity3j.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+var dev_db_url = 'mongodb+srv://store123:store321@cluster0.ity3j.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+var mongoDB = process.env.MONGODB_URI || dev_db_url
 mongoose.connect(mongoDB, {useNewUrlParser: true, useUnifiedTopology: true});
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
